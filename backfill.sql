cf02e10220c5eecc0c6707193adb4cb00d738e34
Rahul Jain <78rkjain@gmail.com>
2026-05-08 11:28:38 -0400

    Remove BOM from backfill.sql so psql can parse it

--- backfill.sql
+++ backfill.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO "people" ("id", "firstName", "lastName", "email", "campaignId", "listSource", "userId", "needsDistrictClassification", "isOutOfDistrict", "createdAt", "updatedAt")
+INSERT INTO "people" ("id", "firstName", "lastName", "email", "campaignId", "listSource", "userId", "needsDistrictClassification", "isOutOfDistrict", "createdAt", "updatedAt")
 SELECT gen_random_uuid(), u."firstName", u."lastName", u."email", cm."campaignId", 'team', cm."userId", true, false, NOW(), NOW()
 FROM "campaign_memberships" cm
 JOIN "users" u ON u."id" = cm."userId"
